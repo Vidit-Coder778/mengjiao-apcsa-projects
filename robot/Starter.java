bed0a5d2fe0522fdc37fc081f2bb63da8df6d777
Vidit-Coder778 <viditkaza@gmail.com>
2025-08-13 22:17:15 +0000

    s

--- robot/Starter.java
+++ robot/Starter.java
@@ -14,7 +14,7 @@ public class Starter implements Directions{
     // The line below creates a Robot that we will refer to as rob
     // Find out what the numbers and direction do!
     // Put rob in a better location for your initials.
-    Robot rob = new Robot(15,2,South,9);
+    Robot rob = new Robot(15,2,South,900);
 
     // Want a second robot?  No prob.  They are cheap :)
     //Robot dude = new Robot(7,5,West,9);
@@ -22,21 +22,40 @@ public class Starter implements Directions{
     rob.move();// move one step in the direction it is facing
 
 
-    // starting the letter R
+    // starting the letter V
     rob.putBeeper();
-    rob.putBeeper();
-    rob.putBeeper();
-    rob.putBeeper();
+    rob.turnLeft();
+    rob.move();
+    rob.turnLeft();
+    rob.turnLeft();
+    rob.turnLeft();
     rob.move();
     rob.putBeeper();
+     rob.turnLeft();
     rob.move();
-    rob.move();
+    rob.turnLeft();
+    rob.turnLeft();
+    rob.turnLeft();
     rob.move();
     rob.putBeeper();
+    rob.turnLeft();
     rob.move();
+    rob.turnLeft();
+    rob.turnLeft();
+    rob.turnLeft();
     rob.move();
     rob.putBeeper();
-
+    rob.turnLeft();
+    rob.move();
+    rob.turnLeft();
+    rob.turnLeft();
+    rob.turnLeft();
+    rob.move();
+    rob.putBeeper();
+    rob.turnLeft();
+    rob.move();
+    
+    
     // done with the line, now on the curve
     // rob.turnLeft();
 
