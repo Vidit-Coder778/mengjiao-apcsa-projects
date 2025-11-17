e70f5bcdc316fa23a62182eb7151959859887bdd
Vidit-Coder778 <viditkaza@gmail.com>
2025-11-17 20:54:01 +0000

    made comments for hand

--- cards/Hand.java
+++ cards/Hand.java
@@ -4,40 +4,42 @@ public class Hand {
 
     private Card[] cards;
     private int length;
-
+    // Constructor takes max number of cards3
     public Hand(int maxCards) {
         cards = new Card[maxCards];
         length = 0;
     }
+    // Adds a card to the hand
     public void add(Card card) {
         if (length < cards.length) {
             cards[length++] = card;
         }
     }
+    // returns number of cards in hand
     public int length() {
         return length;
     }
-
+    // returns card at index without removing it
     public Card get(int index) {
         if (index >= 0 && index < length) {
             return cards[index];
         }
         return null;
     }
-
+    // removes and returns card at index
     public Card remove(int index) {
         if (index < 0 || index >= length) {
             return null;
         }
         Card removed = cards[index];
-
+        // shift cards left to fill gap
         for (int i = index; i < length-1;i++) {
             cards[i] = cards[i+1];
         }
         length--;
         return removed;
     }
-
+    // return the string of all cards
     public String toString() {
         String result = "";
         for (int i = 0; i < length; i++) {
