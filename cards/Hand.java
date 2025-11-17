13ee246d710bc9647c37954329396ff8d38dd406
Vidit-Coder778 <viditkaza@gmail.com>
2025-11-17 18:24:28 +0000

    working on hand

--- cards/Hand.java
+++ cards/Hand.java
@@ -2,4 +2,35 @@ package cards;
 
 public class Hand {
 
+    private Card[] cards;
+    private int length;
+
+    public Hand(int maxCards) {
+        cards = new Card[maxCards];
+        length = 0;
+    }
+    public void add(Card card) {
+        if (length < cards.length) {
+            cards[length++] = card;
+        }
+    }
+    public int length() {
+        return length;
+    }
+
+    public Card get(int index) {
+        if (index >= 0 && index < length) {
+            return cards[index];
+        }
+        return null;
+    }
+
+    public Card remove(int index) {
+        if (index < 0 || index >= length) {
+            return null;
+        }
+        Card removed = cards[index];
+
+        for (int i = index;)
+    }
 }
