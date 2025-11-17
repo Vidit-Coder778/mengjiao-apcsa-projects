a7a12ee2b45754d7d99b02d9018d87fb6e618e1a
Vidit-Coder778 <viditkaza@gmail.com>
2025-11-17 01:40:13 +0000

    finished Card class I believe

--- cards/Card.java
+++ cards/Card.java
@@ -31,7 +31,39 @@ public class Card {
     }
 
     public String toString() {
+
+        String suitSymbol = "";
+            if (suit == 0) {
+                suitSymbol = "♦";
+            }
+            else if (suit == 1) {
+                suitSymbol = "♣"; 
+            } 
+            else if (suit == 2) {
+                suitSymbol = "♥";
+            }
+            else {
+                suitSymbol = "♠";
+            }
         
+        String valueChar = "";
+        if (value == 0) {
+            valueChar = "A";
+        }
+        else if (value >= 1 && value <= 9) {
+            valueChar = String.valueOf(value+1);
+        }
+        else if (value == 10) {
+            valueChar = 10;
+        }
+        else if (value == 11) {
+            valueChar = "Q";
+        }
+        else {
+            valueChar = "K";
+        }
+
+        return suitSymbol + valueChar;
     }
 
 
