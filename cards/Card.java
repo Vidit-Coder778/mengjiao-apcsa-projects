dccc62894bd5707bf54b013a7ad9790dd43014b7
Vidit-Coder778 <viditkaza@gmail.com>
2025-11-17 01:31:25 +0000

    starting the card

--- cards/Card.java
+++ cards/Card.java
@@ -2,5 +2,37 @@ package cards;
 
 public class Card {
 
+    private int suit;
+    private int value;
+
+    public Card(int suit, int value) {
+
+        if (suit < 0) {
+            this.suit = 0;
+        } else if (suit > 3) {
+            this.suit = 3;
+        } else {
+            this.suit = suit;
+        }
+
+         if (value < 0) {
+            this.value = 0;
+        } else if (value > 12) {
+            this.value = 12;
+        } else {
+            this.value = value;
+        }
+
+        
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String toString() {
+        
+    }
+
 
 }
