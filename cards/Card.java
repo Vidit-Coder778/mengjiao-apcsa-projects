dafd987a5f3d8c319235305698fbb6726a1bec28
Vidit-Coder778 <viditkaza@gmail.com>
2025-11-17 22:09:07 +0000

    working, turning in

--- cards/Card.java
+++ cards/Card.java
@@ -4,19 +4,19 @@ public class Card {
     // Instance variables
     private int suit;
     private int value;
-
+    // list of arrays
     private static String[] SUITS = {"♦", "♣", "♥", "♠"};
     private static String[] VALUES = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-
+    // constructor
     public Card(int suit, int value) {
         this.suit = suit;
         this.value = value;
     }
-
+    
     public int getValue() {
         return value;
     }
-
+    // returns string 
     public String toString() {
         return "" + SUITS[suit] + VALUES[value];
     }
