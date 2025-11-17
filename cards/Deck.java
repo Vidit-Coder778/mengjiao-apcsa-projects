74f897f51eea0af699dd805d2948483adb7b45b7
Vidit-Coder778 <viditkaza@gmail.com>
2025-11-17 17:51:03 +0000

    working on deck

--- cards/Deck.java
+++ cards/Deck.java
@@ -1,6 +1,31 @@
 package cards;
 
 public class Deck {
+    private Card[] cards;
+    private int top;
+
+    public Deck() {
+        cards = new Card[52];
+        top = 0;
+        int index = 0;
+        for (int suit = 0;suit < 4; suit++) {
+            for (int value = 0; value < 13; value++) {
+                cards[index] = new Card(suit,value);
+                index++;
+
+            }
+        }
+    }
+
+    public void shuffle() {
+        for (int i = 0; i < 52;i++) {
+            int random = (int)(Math.random * 52);
+            Card temp = cards[i];
+            cards[i] = cards[random];
+            cards[random] = temp;
+        }
+        top = 0;
+    }
 
 
 }
