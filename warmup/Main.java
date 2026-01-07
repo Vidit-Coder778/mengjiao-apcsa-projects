273e1fdc32fbc7d6bf78ac7fada710c7583d702b
Vidit-Coder778 <viditkaza@gmail.com>
2026-01-07 23:00:09 +0000

    finished first 2 for loops

--- warmup/Main.java
+++ warmup/Main.java
@@ -4,6 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         // My code
+        int sum = 0;
+
+        for (int i = 3; i < 1000; i = i+3) {
+            sum = sum + i;
+        }
+
+        for (int i = 5; i < 1000, i = i + 5) {
+            sum = sum + i;
+        }
+
+        
+
+
 
     }
 }
