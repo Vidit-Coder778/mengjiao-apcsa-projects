2bff38c7b33a90b459d9e5d1a773dc5ad14517bb
Vidit-Coder778 <viditkaza@gmail.com>
2026-01-27 21:56:38 +0000

    start project

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -4,5 +4,17 @@ public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
+        int n = input.length();
+        for (int i = 1, i < n; i++) {
+            int key = input[i];
+            int j = i-1;
+
+            while (j >= 0 && input[j] > key) {
+                input[j+1] = input[j];
+            }
+        }
     }
 }
+
+
+
