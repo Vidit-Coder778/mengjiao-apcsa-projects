0020f445f181fd2397048a18e6c1bc943f944d53
Vidit-Coder778 <viditkaza@gmail.com>
2026-01-28 15:26:27 +0000

    start selection sort

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -4,5 +4,11 @@ public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
+        int n = input.length();
+        for (int i = 0; i < n; i++) {
+
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++)
+        }
     }
 }
