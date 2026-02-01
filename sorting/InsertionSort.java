67681580ac848c5db70fd846e9c29d48973ae930
Vidit-Coder778 <viditkaza@gmail.com>
2026-02-01 21:09:04 +0000

    making final changes

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -3,7 +3,7 @@ package sorting;
 public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
-        System.out.println("Insertion Sort!!!");
+        
         int n = input.length;
         for (int i = 1; i < n; i++) {
             int key = input[i];
--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -3,7 +3,7 @@ package sorting;
 public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
-        System.out.println("Selection Sort!!!");
+        
         int n = input.length;
         for (int i = 0; i < n; i++) {
 
